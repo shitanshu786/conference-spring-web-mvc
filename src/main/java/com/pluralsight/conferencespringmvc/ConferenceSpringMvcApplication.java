2e5af0e18c3def30c17e030c2503f94ea7b2e2f9
Shitanshu(Shittu) <shitanshu.student.eee14@itbhu.ac.in>
2020-04-14 09:38:48 +0530

    Added configuration for WAR deployment on tomcat server and added initial jsp

--- src/main/java/com/pluralsight/conferencespringmvc/ConferenceSpringMvcApplication.java
+++ src/main/java/com/pluralsight/conferencespringmvc/ConferenceSpringMvcApplication.java
@@ -2,9 +2,10 @@ package com.pluralsight.conferencespringmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ConferenceSpringMvcApplication {
+public class ConferenceSpringMvcApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConferenceSpringMvcApplication.class, args);
