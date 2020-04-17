d17d696409d6248569cdbd5a99d78fe93fa1a43e
Shitanshu(Shittu) <shitanshu.student.eee14@itbhu.ac.in>
2020-04-17 11:10:02 +0530

    upto I18N internationalization interceptor

---
+++ src/main/java/com/pluralsight/conferencespringmvc/controllers/RegistrationController.java
@@ -0,0 +1,23 @@
+package com.pluralsight.conferencespringmvc.controllers;
+
+import com.pluralsight.conferencespringmvc.models.Registration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("registration")
+    public String getRegistration(@ModelAttribute("registration")Registration registration){
+        return "registration";
+    }
+
+    @PostMapping("registration")
+    public String addRegistration(@ModelAttribute("registration")Registration registration){
+        return "registration";
+    }
+}
--- src/main/webapp/WEB-INF/jsp/greeting.jsp
+++ src/main/webapp/WEB-INF/jsp/greeting.jsp
@@ -1,7 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
 <head>
-    <meta charset="UTF-8">
     <title>Greetings</title>
 </head>
 <body>
